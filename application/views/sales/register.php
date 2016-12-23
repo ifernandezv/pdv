0f0a032cbda3cbbf48a6926476fcb4ae4c3df907
pablo <pablo@koala-soft.com>
2016-12-23 12:03:45 -0400

    borrado de geolocation duplicado

--- application/views/sales/register.php
+++ application/views/sales/register.php
@@ -1389,30 +1389,6 @@ function updateInfo (marker) {
    document.getElementById('longsale').setAttribute("value",markerLatLng.lng()); 
 }
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-  //    infoWindow.setPosition(pos);
-  //    infoWindow.setContent('Location found.');
-  //    map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}  
-
 function initialize() {
   navigator.geolocation.getCurrentPosition(function(position) { 
   lat1 = position.coords.latitude;
